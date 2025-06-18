448fd915dd33233f5e97e0d7c1c2cc457e0c17eb
davidwilby <24752124+davidwilby@users.noreply.github.com>
2025-06-18 17:02:13 +0100

    allow copernicusmarine creds to be set by environment variables

--- scripts/data_get_credentials.sh
+++ scripts/data_get_credentials.sh
@@ -5,9 +5,9 @@ set -e
 copernicus_credentials_dir="$HOME/.copernicusmarine"
 copernicus_credentials_file="$copernicus_credentials_dir/.copernicusmarine-credentials"
 
-# CopernicusMarine Credentials
-if [ -f "$copernicus_credentials_file" ]; then
-    echo "Using copernicusmarine credentials from $copernicus_credentials_file"
+# CopernicusMarine Credentials - use environment variables as priority, then copernicus credentials file format, fall back on old user and password files
+if [[ -n "$COPERNICUSMARINE_SERVICE_USERNAME" ]] && [[ -n "$COPERNICUSMARINE_SERVICE_PASSWORD" ]] || [ -f "$copernicus_credentials_file" ]; then
+    copernicusmarine login --check-credentials-valid
 elif [ -f "$copernicus_credentials_dir/user" ] && [ -f "$copernicus_credentials_dir/password" ]; then
     COPERNICUS_USER=$(< "${HOME}"/.copernicusmarine/user)
     COPERNICUS_PASSWORD=$(< "${HOME}"/.copernicusmarine/password)
@@ -15,9 +15,9 @@ elif [ -f "$copernicus_credentials_dir/user" ] && [ -f "$copernicus_credentials_
     copernicusmarine login \
     --username "$COPERNICUS_USER" \
     --password "$COPERNICUS_PASSWORD"
-
 else
-    echo "ERROR - No credentials found for copernicusmarine. Please log in using copernicusmarine login"
+    echo "ERROR - Username and password not found for copernicusmarine. Please log in using copernicusmarine login \
+        or set COPERNICUSMARINE_SERVICE_USERNAME and COPERNICUSMARINE_SERVICE_PASSWORD environment variables."
     exit 1
 fi
 
